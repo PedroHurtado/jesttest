7c4d69fb444ab203d8e77e9eb555a3a04c5b5993
PedroHurtado <pedrohurt@gmail.com>
2018-08-06 15:15:24 +0200

    add describe section to fs.test.js

--- src/tfidf/__test__/fs.test.js
+++ src/tfidf/__test__/fs.test.js
@@ -1,51 +1,69 @@
-const {isFile, readFile, readDir} = require('../fs');
+const { isFile, readFile, readDir } = require('../fs');
 
 jest.mock('fs');
 
 describe('fs promises', () => {
-  test('isFile is file', async () => {
-    let result = await isFile('file');
-    expect(result).toBe(true);
-  });
-  test('isFile is folder', async () => {
-    let result = await isFile('./folder');
-    expect(result).toBe(false);
-  });
-  test('isFile file not exists', async () => {
-    try {
-      await isFile('');
-    } catch (e) {
-      expect(e).toEqual('error');
-    }
+
+  describe('isFile', () => {
+
+    test('Is file', async () => {
+      let result = await isFile('file');
+      expect(result).toBe(true);
+    });
+
+    test('Is folder', async () => {
+      let result = await isFile('./folder');
+      expect(result).toBe(false);
+    });
+
+    test('File not exists', async () => {
+      try {
+        await isFile('');
+      } catch (e) {
+        expect(e).toEqual('error');
+      }
+    });
+
   });
 
-  test('readFile file exists', async () => {
-    let content = await readFile('OK');
-    expect(content).toEqual('data');
+  describe('readFile', () => {
+
+    test('file exists', async () => {
+      let content = await readFile('OK');
+      expect(content).toEqual('data');
+    });
+
+    test('readFile file not exists', async () => {
+      try {
+        await readFile('');
+      } catch (e) {
+        expect(e).toEqual('error');
+      }
+    });
+
   });
 
-  test('readFile file not exists', async () => {
-    try {
-      await readFile('');
-    } catch (e) {
-      expect(e).toEqual('error');
-    }
+  describe('readDir', () => {
+
+    test('readDir path exists', async () => {
+      let content = await readDir('.');
+      let checkedDocuments = [
+        './a.txt',
+        './empty',
+        './a.txt',
+        ['./folder/b.txt'],
+      ];
+      expect(content).toEqual(checkedDocuments);
+    });
+
+    test('readDir path not exists', async () => {
+      try {
+        await readDir('');
+      } catch (e) {
+        expect(e).toEqual('error');
+      }
+    });
+
   });
-  test('readDir path exists', async () => {
-    let content = await readDir('.');
-    let checkedDocuments = [
-      './a.txt',
-      './empty',
-      './a.txt',
-      ['./folder/b.txt'],
-    ];
-    expect(content).toEqual(checkedDocuments);
-  });
-  test('readDir path not exists', async () => {
-    try {
-      await readDir('');
-    } catch (e) {
-      expect(e).toEqual('error');
-    }
-  });
+
 });
